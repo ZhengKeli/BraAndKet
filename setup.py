f3605365938b8ed2d84b5bca7a1751fd3857938f
keli <zhengkeli2009@126.com>
2021-06-09 11:21:18 +0800

    v0.7.0

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name="BraAndKet",
-    version="0.6.3",
+    version="0.7.0",
     author="Zheng Keli",
     author_email="zhengkeli2009@126.com",
     description="BraAndKet is a library for numeral calculations of discrete quantum systems.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ZhengKeli/BraAndKet",
-    packages=['braandket'],
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
