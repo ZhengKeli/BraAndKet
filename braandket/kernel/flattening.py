979b8169828fe942ed76684aff2da8ab3725bba6
keli <zhengkeli2009@126.com>
2021-06-23 13:38:10 +0800

    added kernel mixin Schrodinger, which automatically removes deco in model

--- braandket/kernel/flattening.py
+++ braandket/kernel/flattening.py
@@ -58,26 +58,33 @@ class FlatteningRho(Flattening, abc.ABC):
 
 # schrodinger pade
 
-class SchrodingerPsiPade(Static, FlatteningPsi):
+class Schrodinger(Static, Flattening, abc.ABC):
+    @classmethod
+    def init_model(cls, model, value):
+        (hb, hmt, _), value, wrapping = super().init_model(model, value)
+        return (hb, hmt), value, wrapping
+
+
+class SchrodingerPsiPade(Schrodinger, Static, FlatteningPsi):
     def __init__(self, model, time, value):
         super().__init__(model, time, value)
 
     @classmethod
     def compute_static(cls, model, psi, span, **kwargs):
-        hb, hmt, _ = model
+        hb, hmt = model
 
         from scipy.linalg import expm
         op = expm((span / 1j / hb) * hmt)
         return op @ psi
 
 
-class SchrodingerRhoPade(Static, FlatteningRho):
+class SchrodingerRhoPade(Schrodinger, Static, FlatteningRho):
     def __init__(self, model, time, value):
         super().__init__(model, time, value)
 
     @classmethod
     def compute_static(cls, model, rho, span, **kwargs):
-        hb, hmt, _ = model
+        hb, hmt = model
 
         from scipy.linalg import expm
         op = expm((span / 1j / hb) * hmt)
