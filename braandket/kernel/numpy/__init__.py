4fb54cf56726ccf695504217ad7c241e7e351760
keli <zhengkeli2009@126.com>
2021-06-23 14:10:06 +0800

    added euler and rk4 support for schrodinger evolving in new evolution framework

--- braandket/kernel/numpy/__init__.py
+++ braandket/kernel/numpy/__init__.py
@@ -1,2 +1,11 @@
-from .static_lindblad import NumpyStaticLindbladEulerKernel, NumpyStaticLindbladRk4Kernel
-from .static_schrodinger import NumpyStaticSchrodingerPsiPadeKernel, NumpyStaticSchrodingerRhoPadeKernel
+from .static_lindblad import \
+    NumpyStaticLindbladEulerKernel, \
+    NumpyStaticLindbladRk4Kernel
+
+from .static_schrodinger import \
+    NumpyStaticSchrodingerPsiPadeKernel, \
+    NumpyStaticSchrodingerRhoPadeKernel, \
+    NumpyStaticSchrodingerPsiEuler, \
+    NumpyStaticSchrodingerRhoEuler, \
+    NumpyStaticSchrodingerPsiRk4, \
+    NumpyStaticSchrodingerRhoRk4
--- braandket/kernel/numpy/static_schrodinger.py
+++ braandket/kernel/numpy/static_schrodinger.py
@@ -1,7 +1,7 @@
 import abc
 
 from .numpy import NumpyMixin, NumpyPsiMixin, NumpyRhoMixin
-from ..abstract import Static
+from ..abstract import Static, StaticStepping
 
 
 # mixin
@@ -11,6 +11,10 @@ class NumpyStaticSchrodingerMixin(Static, NumpyMixin, abc.ABC):
     def init_model(cls, model, value):
         (hb, hmt, _), value, wrapping = super().init_model(model, value)
         return (hb, hmt), value, wrapping
+
+
+class NumpyStaticSchrodingerSteppingMixin(NumpyStaticSchrodingerMixin, StaticStepping, abc.ABC):
+    pass
 
 
 # kernels
@@ -39,3 +43,76 @@ class NumpyStaticSchrodingerRhoPadeKernel(NumpyStaticSchrodingerMixin, NumpyRhoM
         from scipy.linalg import expm
         op = expm((span / 1j / hb) * hmt)
         return op @ rho @ op
+
+
+class NumpyStaticSchrodingerPsiEuler(NumpyStaticSchrodingerSteppingMixin, NumpyPsiMixin):
+    def __init__(self, model, time, value, *, dt=None):
+        super().__init__(model, time, value, dt=dt)
+
+    @classmethod
+    def compute_static_stepping(cls, model, psi, n, dt, **kwargs):
+        hb, hmt = model
+        kt = (dt / 1j / hb)
+
+        for i in range(n):
+            psi += kt * (hmt @ psi)
+
+        return psi
+
+
+class NumpyStaticSchrodingerRhoEuler(NumpyStaticSchrodingerSteppingMixin, NumpyRhoMixin):
+    def __init__(self, model, time, value, *, dt=None):
+        super().__init__(model, time, value, dt=dt)
+
+    @classmethod
+    def compute_static_stepping(cls, model, rho, n, dt, **kwargs):
+        hb, hmt = model
+        kt = (dt / 1j / hb)
+
+        for i in range(n):
+            rho += kt * (hmt @ rho - rho @ hmt)
+
+        return rho
+
+
+class NumpyStaticSchrodingerPsiRk4(NumpyStaticSchrodingerSteppingMixin, NumpyPsiMixin):
+    def __init__(self, model, time, value, *, dt=None):
+        super().__init__(model, time, value, dt=dt)
+
+    @classmethod
+    def compute_static_stepping(cls, model, psi, n, dt, **kwargs):
+        hb, hmt = model
+        k = -1j / hb
+        dt2 = dt / 2.0
+
+        for i in range(n):
+            k1 = k * (hmt @ psi)
+            k2 = k * (hmt @ (psi + dt2 * k1))
+            k3 = k * (hmt @ (psi + dt2 * k2))
+            k4 = k * (hmt @ (psi + dt * k3))
+            psi += dt / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+        return psi
+
+
+class NumpyStaticSchrodingerRhoRk4(NumpyStaticSchrodingerSteppingMixin, NumpyRhoMixin):
+    def __init__(self, model, time, value, *, dt=None):
+        super().__init__(model, time, value, dt=dt)
+
+    @classmethod
+    def compute_static_stepping(cls, model, rho, n, dt, **kwargs):
+        hb, hmt = model
+        k = -1j / hb
+        dt2 = dt / 2.0
+
+        for i in range(n):
+            k1 = k * (hmt @ rho - rho @ hmt)
+            rho1 = rho + dt2 * k1
+            k2 = k * (hmt @ rho1 - rho1 @ hmt)
+            rho2 = rho + dt2 * k2
+            k3 = k * (hmt @ rho2 - rho2 @ hmt)
+            rho3 = rho + dt * k3
+            k4 = k * (hmt @ rho3 - rho3 @ hmt)
+            rho += dt / 6.0 * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+        return rho
