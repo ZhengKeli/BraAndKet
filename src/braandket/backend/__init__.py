4ea3d5faf7fca49121102a79834e76948acac913
keli <zhengkeli2009@126.com>
2023-02-13 12:07:41 +0800

    catch ImportError when importing backends

--- src/braandket/backend/__init__.py
+++ src/braandket/backend/__init__.py
@@ -1,4 +1,12 @@
 from .backend import Backend, ValuesType
 from .default import get_default_backend, set_default_backend
-from .numpy_backend import NumpyBackend, numpy_backend
-from .tensorflow_backend import TensorflowBackend, tensorflow_backend
+
+try:
+    from .numpy_backend import NumpyBackend, numpy_backend
+except ImportError:
+    pass
+
+try:
+    from .tensorflow_backend import TensorflowBackend, tensorflow_backend
+except ImportError:
+    pass
