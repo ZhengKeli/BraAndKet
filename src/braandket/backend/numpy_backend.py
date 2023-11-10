fixed the mistake in measure_pure_state() that the prob in normalization is not wrapped in sqrt

src/braandket/backend/numpy_backend.py
@@ -208,7 +208,7 @@ class NumpyBackend(Backend[np.ndarray]):
         # [batches_n]
         chosen_component = state[np.arange(batches_n), choice]
         # [batches_n, reduced_n]
-        chosen_component /= np.expand_dims(chosen_prob, axis=-1)  # normalization
+        chosen_component /= np.expand_dims(np.sqrt(chosen_prob), axis=-1)  # normalization
         # [batches_n, reduced_n]
 
         chosen_onehot = np.arange(choices_n, dtype=choice.dtype) == np.expand_dims(choice, -1)

src/braandket/backend/tensorflow_backend.py
@@ -245,7 +245,7 @@ class TensorflowBackend(Backend[tf.Tensor]):
         chosen_component = tf.gather_nd(state, chosen_gather_indices)
         # [batches_n, reduced_n]
         chosen_component /= tf.cast(
-            tf.expand_dims(chosen_prob, axis=-1),
+            tf.expand_dims(tf.sqrt(chosen_prob), axis=-1),
             dtype=chosen_component.dtype)  # normalization
         # [batches_n, reduced_n]
 